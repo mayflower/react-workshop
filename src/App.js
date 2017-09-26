5873c4324c03209f21c128c54dfe6150554f5e22
Florian Fackler <florian.fackler@mintao.com>
2017-09-26 11:44:18 +0200

    Change color by number of clicks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { incrementCounter, decrementCounter } from "./actions";
 import FlatButton from "material-ui/FlatButton";
-import { Card, CardTitle, CardText } from "material-ui/Card";
+import { Card, CardTitle, CardText, CardActions } from "material-ui/Card";
+import {
+  red50,
+  red100,
+  red200,
+  red300,
+  red400,
+  red500,
+  red600,
+  red700,
+  red800,
+  red900
+} from "material-ui/styles/colors";
+
+const colors = [
+  red50,
+  red100,
+  red200,
+  red300,
+  red400,
+  red500,
+  red600,
+  red700,
+  red800,
+  red900
+];
 
 const App = props => {
-  const headlineStyle = {
-    display: props.value > 0 ? "block" : "none"
+  const infoStyle = {
+    visibility: props.value > 0 ? "visible" : "hidden",
+    color: colors[props.value - 1]
   };
   return (
     <Card>
       <CardTitle title="Simple click counter" subtitle="Click - or + button" />
       <CardText>
-        <p style={headlineStyle}>Du hast schon {props.value} mal geklick</p>
+        <p style={infoStyle}>Du hast schon {props.value} mal geklick</p>
+      </CardText>
+      <CardActions>
         <FlatButton onClick={props.onDecrement} label="-" />
         <FlatButton primary onClick={props.onIncrement} label="+" />
-      </CardText>
+      </CardActions>
     </Card>
   );
 };
